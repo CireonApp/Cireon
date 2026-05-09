3bd154082bd1704be9bd1f21cf90fd7923a18764
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-05-09 18:37:13 +0300

    Fix imidiate crash of pom resource on builds.

--- src/main/java/com/cireonapp/server/ServerApplication.java
+++ src/main/java/com/cireonapp/server/ServerApplication.java
@@ -8,8 +8,10 @@ import com.cireonapp.server.initializer.SourceSubscription;
 import com.cireonapp.server.util.DataDirHelper;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.FileReader;
@@ -20,37 +22,27 @@ import java.util.logging.Logger;
 
 @SpringBootApplication
 public class ServerApplication {
-
     private static ConfigurableApplicationContext application;
 
     public static Logger LOGGER = Logger.getLogger("Cireon Backend Server");
 
     public static void main(String[] args) throws Exception {
-        MavenXpp3Reader reader = new MavenXpp3Reader();
-        Model model = reader.read(new FileReader("pom.xml"));
-
-
         DataDirHelper.initializeDataDir();
-
         Databases.bootstrap();
         Config config = ConfigManager.get();
         Databases.shutdown();
 
         SpringApplication app = new SpringApplication(ServerApplication.class);
-
         app.addInitializers(new Databases());
 
+        // Only set dynamic runtime properties here, NOT the banner info
         Map<String, Object> props = new HashMap<>();
         props.put("server.port", config.getPort());
-        props.put("application.title", model.getName());
-        props.put("application.version", model.getVersion());
-
         app.setDefaultProperties(props);
-        app.addInitializers(new FileWatcher(), new SourceSubscription());
 
+        app.addInitializers(new FileWatcher(), new SourceSubscription());
         application = app.run(args);
     }
-
 
     public static void restart() {
         application.restart();
