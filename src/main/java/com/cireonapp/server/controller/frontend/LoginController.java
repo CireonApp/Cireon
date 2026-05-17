7a2f02c648fa3bad09d607d13db5b9557b0b50ab
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-05-18 01:11:05 +0300

    Add theme label to login model and prepare for future setup redirection

--- src/main/java/com/cireonapp/server/controller/frontend/LoginController.java
+++ src/main/java/com/cireonapp/server/controller/frontend/LoginController.java
@@ -1,6 +1,7 @@
 package com.cireonapp.server.controller.frontend;
 
 import com.cireonapp.server.domain.session.SessionManager;
+import com.cireonapp.server.domain.user.UserManager;
 import com.cireonapp.server.util.CookieHelper;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,6 +16,13 @@ import java.util.Optional;
 public class LoginController {
     @GetMapping("/login")
     String login(Model model, HttpServletRequest request, HttpServletResponse response) {
+
+//        if(UserManager.getCount() == 0){
+//            return "redirect:/setup";
+//        setup does not exists yet! For future create one and redirect.
+//        }
+
+        model.addAttribute("theme", UserManager.getThemeLabel(null).label);
 
         Optional<Cookie> optCookie = CookieHelper.getAuthCookie(request);
 
