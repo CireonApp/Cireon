2c82c86e2f1c230fc6bdbfafbe71087a012893dd
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-05-24 00:22:05 +0300

    movie parseName function to the ParseName class and fix year and title problem.

--- src/main/java/com/cireonapp/server/domain/media/common/ParsedName.java
+++ src/main/java/com/cireonapp/server/domain/media/common/ParsedName.java
@@ -1,5 +1,7 @@
 package com.cireonapp.server.domain.media.common;
 
+import java.nio.file.Path;
+
 public class ParsedName {
     private String name;
     private String year;
@@ -34,5 +36,35 @@ public class ParsedName {
                 "name='" + name + '\'' +
                 ", year='" + year + '\'' +
                 '}';
+    }
+
+    public static ParsedName parseName(Path filePath) {
+        String file = filePath.getFileName().toString();
+        file = file.replaceFirst("\\.[^.]+$", "");
+        file = file.replaceAll("(?i)\\b(1337x|thepiratebay|tpb|torrentgalaxy|tgx|limetorrents|magnetdl|yts|yify|nyaa|fitgirl|repacks|eztv|rutracker|zooqle|kickasstorrents|kat|torrentz2|btdig|snowfl|animetosho|myanonamouse|mam|academictorrents|tamilrockers|extto|torlock|torrentdownloads|yourbittorrent|rarbg|rutor|ibit|demonoid|pirateiro|kinozal|btmet|qbittorrent|deluge|transmission|utorrent|biglybt|tixati|vuze)\\b", "");
+        file = file.replaceAll("(?i)\\b(1080p|720p|2160p|4k|bluray|brrip|webrip|x264|x265|h264|h265)\\b", "");
+        file = file.replaceAll("\\[.*?]", "");
+        file = file.replaceAll("\\.", " ");
+        file = file.trim();
+
+        ParsedName result = new ParsedName();
+
+        var match = file.matches(".*\\(\\d{4}\\)$");
+
+        if (match) {
+            int idx = file.lastIndexOf('(');
+            result.setName(file.substring(0, idx).trim());
+            result.setYear(file.substring(idx + 1, idx + 5));
+            return result;
+        }
+
+        var yearMatcher = java.util.regex.Pattern.compile("(\\d{4})").matcher(file);
+        if (yearMatcher.find()) {
+            result.setYear(yearMatcher.group(1));
+            file = file.substring(0, yearMatcher.start());
+        }
+
+        result.setName(file.trim());
+        return result;
     }
 }
