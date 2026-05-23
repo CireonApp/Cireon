677d2746c9954a6fd20e7f549103d3e54ad177e5
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-05-24 00:10:35 +0300

    Generalize StreamingService and allow for static files from classpath

--- src/main/java/com/cireonapp/server/service/ContentService.java
+++ src/main/java/com/cireonapp/server/service/ContentService.java
@@ -1,6 +1,5 @@
 package com.cireonapp.server.service;
 
-import com.cireonapp.server.ServerApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -8,24 +7,26 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 
 @Service
-public class StreamingService {
+public class ContentService {
     @Autowired
     private ResourceLoader resourceLoader;
 
-    public Mono<Resource> getVideo(String videoPath) {
-        Path path = Path.of(videoPath);
+    public Mono<Resource> getContent(String contentPath) {
+        Path path = Path.of(contentPath);
 
         if (!Files.exists(path) || !Files.isReadable(path)) {
-            ServerApplication.LOGGER.warn("File not found: " + videoPath);
-            return Mono.error(new FileNotFoundException("File not found: " + videoPath));
+            return Mono.error(new FileNotFoundException("File not found: " + contentPath));
         }
 
         return Mono.fromSupplier(()->resourceLoader.getResource(path.toUri().toString()));
+    }
+
+    public Mono<Resource> getClasspathContent(String resourcePath) {
+        return Mono.fromSupplier(() -> resourceLoader.getResource("classpath:" + resourcePath));
     }
 }
