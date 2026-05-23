55fedc77fecb396fd4f199d0b567edae3841bd76
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-05-24 00:15:51 +0300

    Allow username to be set in advance in query param.

--- src/main/java/com/cireonapp/server/controller/frontend/LoginController.java
+++ src/main/java/com/cireonapp/server/controller/frontend/LoginController.java
@@ -9,13 +9,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    String login(Model model, HttpServletRequest request, HttpServletResponse response) {
+    String login(Model model, HttpServletRequest request, @RequestParam(name = "username", required = false) String username) {
 
 //        if(UserManager.getCount() == 0){
 //            return "redirect:/setup";
@@ -26,11 +27,15 @@ public class LoginController {
 
         Optional<Cookie> optCookie = CookieHelper.getAuthCookie(request);
 
-        if(optCookie.isPresent()){
+        if (optCookie.isPresent()) {
             Cookie cookie = optCookie.get();
-            if(SessionManager.isValid(cookie.getValue())){
+            if (SessionManager.isValid(cookie.getValue())) {
                 return "redirect:/";
             }
+        }
+
+        if (username != null) {
+            model.addAttribute("usernameInput", username);
         }
 
         return "login/index";
