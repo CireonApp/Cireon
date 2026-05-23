ba771e240ee8e65859f0a7fa2dea5b7e195af18c
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-05-24 00:24:36 +0300

    Add logging for SourceSubscription initializer.

--- src/main/java/com/cireonapp/server/initializer/SourceSubscription.java
+++ src/main/java/com/cireonapp/server/initializer/SourceSubscription.java
@@ -1,5 +1,6 @@
 package com.cireonapp.server.initializer;
 
+import com.cireonapp.server.ServerApplication;
 import com.cireonapp.server.domain.media.source.Source;
 import com.cireonapp.server.domain.media.source.SourceManager;
 import org.springframework.context.ApplicationContextInitializer;
@@ -10,9 +11,10 @@ import java.util.Set;
 public class SourceSubscription implements ApplicationContextInitializer<ConfigurableApplicationContext> {
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
+        ServerApplication.LOGGER.info("Subscribing to source changes...");
         Set<Source> sources = SourceManager.getAll(true, true);
         for (Source source : sources) {
-            FileWatcher.RegisterPath(source);
+            FileWatcher.registerSource(source);
         }
     }
 }
