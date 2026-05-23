90ca051645ea815e069b2eb2b7db9c3cf40619a2
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-05-24 00:08:34 +0300

    Billboard class information

---
+++ src/main/java/com/cireonapp/server/domain/media/common/Billboard.java
@@ -0,0 +1,12 @@
+package com.cireonapp.server.domain.media.common;
+
+public class Billboard {
+    public String id;
+    public String sourceType;
+    public Artwork artwork;
+    public String name;
+    public String[] genres;
+    public boolean isAdult;
+    public int runtime;
+    public String releaseDate;
+}
