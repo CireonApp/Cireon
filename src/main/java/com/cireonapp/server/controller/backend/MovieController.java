c96104163491b7042d749f5ee4e3f4143724f08b
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-05-27 14:48:12 +0300

    Add user authentication check to movie search endpoint

--- src/main/java/com/cireonapp/server/controller/backend/MovieController.java
+++ src/main/java/com/cireonapp/server/controller/backend/MovieController.java
@@ -3,7 +3,14 @@ package com.cireonapp.server.controller.backend;
 import com.cireonapp.server.domain.media.common.SearchResults;
 import com.cireonapp.server.domain.media.movie.Movie;
 import com.cireonapp.server.domain.media.movie.MovieManager;
+import com.cireonapp.server.domain.user.User;
+import com.cireonapp.server.dto.CommonResponseDto;
 import com.cireonapp.server.dto.MovieResponseDto;
+import com.cireonapp.server.util.CookieHelper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Tag(name = "Movies API", description = "Movies related endpoints")
 @RestController
 @RequestMapping("/api/movie")
 public class MovieController {
+    @Operation(
+            summary = "Simple movie Search.",
+            description = "Search movies by title. Returns a list of movies that match the query with scoring."
+    )
     @GetMapping("/search")
-    public ResponseEntity<List<SearchResults<MovieResponseDto>>> getAll(@RequestParam(value = "query",defaultValue = "") String query) {
+    public ResponseEntity<?> getAll(HttpServletRequest request, @RequestParam(value = "query", defaultValue = "") String query) {
+        Optional<User> user = CookieHelper.getUserFromSessionCookie(request);
+
+        if (user.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(CommonResponseDto.Error.NOT_LOGGED_IN);
+        }
+
         List<SearchResults<Movie>> searchResults = MovieManager.search(query, 15);
         List<SearchResults<MovieResponseDto>> results = new ArrayList<>();
         searchResults.forEach(result -> {
