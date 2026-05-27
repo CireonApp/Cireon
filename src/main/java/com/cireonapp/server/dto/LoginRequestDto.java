a8ba9548bef55e91dafab7f6ecf041cef50bc96c
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-05-27 15:01:09 +0300

    Add Dto for login request to use instead of Authentication header.

---
+++ src/main/java/com/cireonapp/server/dto/LoginRequestDto.java
@@ -0,0 +1,20 @@
+package com.cireonapp.server.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class LoginRequestDto {
+    @NotNull(message="Username is required")
+    @NotBlank(message="Username is required")
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Username can only contain letters, numbers, and underscores.")
+    @Size(min = 6,max = 18, message = "Username must be between 6 and 18 characters.")
+    public String username;
+
+    @NotNull(message="Password is required")
+    @NotBlank(message="Password is required")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Password can only contain letters and numbers.")
+    @Size(min = 8,max=24, message = "Password must be at least 8 characters long.")
+    public String password;
+}
