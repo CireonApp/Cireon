a7ead2b9a0eddf0f4a1ef74ba30e4b60a0c6f639
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-05-27 15:01:45 +0300

    Refactor SourceSubscription to use Cursor for source retrieval instead of Set

--- src/main/java/com/cireonapp/server/initializer/SourceSubscription.java
+++ src/main/java/com/cireonapp/server/initializer/SourceSubscription.java
@@ -3,17 +3,17 @@ package com.cireonapp.server.initializer;
 import com.cireonapp.server.ServerApplication;
 import com.cireonapp.server.domain.media.source.Source;
 import com.cireonapp.server.domain.media.source.SourceManager;
+import org.dizitart.no2.repository.Cursor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.Set;
 
 public class SourceSubscription implements ApplicationContextInitializer<ConfigurableApplicationContext> {
     @Override
     public void initialize(@NonNull ConfigurableApplicationContext applicationContext) {
         ServerApplication.LOGGER.info("Subscribing to source changes...");
-        Set<Source> sources = SourceManager.getAll(true, true);
+        Cursor<Source> sources = SourceManager.getAll(true, true);
         for (Source source : sources) {
             FileWatcher.registerSource(source);
         }
