a600dd2bd8368e6daa5adcca116493fa962a2e59
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-05-27 14:56:27 +0300

    Refactor SignupController to improve user session handling and redirect logic

--- src/main/java/com/cireonapp/server/controller/frontend/SignupController.java
+++ src/main/java/com/cireonapp/server/controller/frontend/SignupController.java
@@ -1,10 +1,9 @@
 package com.cireonapp.server.controller.frontend;
 
-import com.cireonapp.server.domain.session.SessionManager;
+import com.cireonapp.server.domain.user.User;
+import com.cireonapp.server.domain.user.UserManager;
 import com.cireonapp.server.util.CookieHelper;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +13,15 @@ import java.util.Optional;
 @Controller
 public class SignupController {
     @GetMapping("/signup")
-    String signup(Model model, HttpServletRequest request, HttpServletResponse response) {
+    String signup(Model model, HttpServletRequest request) {
+        Optional<User> user = CookieHelper.getUserFromSessionCookie(request);
 
-        Optional<Cookie> optCookie = CookieHelper.getAuthCookie(request);
-
-        if(optCookie.isPresent()){
-            Cookie cookie = optCookie.get();
-            if(SessionManager.isValid(cookie.getValue())){
-                return "redirect:/";
-            }
+        if (user.isEmpty()) {
+            return "redirect:/login";
         }
+
+
+        model.addAttribute("theme", UserManager.getThemeLabel(null).label);
 
         return "signup/index";
     }
