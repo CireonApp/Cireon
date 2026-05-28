d2417e4261ad7bce5a4245f6a2b2137ddc8c4362
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-05-29 01:17:36 +0300

    Update validation annotations in LoginRequestDto for password field

--- src/main/java/com/cireonapp/server/dto/LoginRequestDto.java
+++ src/main/java/com/cireonapp/server/dto/LoginRequestDto.java
@@ -18,12 +18,9 @@ public class LoginRequestDto {
     @Schema(description = "Username of the user you want to log into", defaultValue = "john_doe")
     public String username;
 
-    @NotNull(message="Password is required")
-    @NotBlank(message="Password is required")
     @NotNull(message = "Password is required")
     @NotBlank(message = "Password is required")
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Password can only contain letters and numbers.")
-    @Size(min = 8,max=24, message = "Password must be at least 8 characters long.")
     @Schema(description = "Password of the user you want to log into", defaultValue = "johndoetheking123")
     @Size(min = 8, max = 24, message = "Password must be at least 8 characters long.")
     public String password;
