84175cd820b9a7250c0ec4ead2005cd81f6b040a
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-06-01 23:01:46 +0300

    Add interceptor for last use for users on page visit or api use.

--- src/main/java/com/cireonapp/server/service/WebMvcConfig.java
+++ src/main/java/com/cireonapp/server/service/WebMvcConfig.java
@@ -1,7 +1,9 @@
 package com.cireonapp.server.service;
 
+import com.cireonapp.server.service.interceptor.UpdateUserLastUse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +11,19 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UpdateUserLastUse()).addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/assets/**",
+                        "/favicon.ico",
+                        "/error",
+                        "/actuator/**"
+                );
+//            registry.addInterceptor(new FirstTimeSetupInterceptor())
+//                    .addPathPatterns("/login","/","/signup","/admin");
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/assets/fonts/**")
