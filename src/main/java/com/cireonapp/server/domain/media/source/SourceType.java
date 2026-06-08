7162076c6dad8e3523b8f700b48caa628be069b5
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-06-08 22:32:58 +0300

    Add singular for source type enum.

--- src/main/java/com/cireonapp/server/domain/media/source/SourceType.java
+++ src/main/java/com/cireonapp/server/domain/media/source/SourceType.java
@@ -1,18 +1,24 @@
 package com.cireonapp.server.domain.media.source;
 
 public enum SourceType {
-    MOVIE("movie"),
-    TV_SHOW("tv_show"),
-    MUSIC("music"),
-    BOOK("book");
+    MOVIE("Movies","movie"),
+    TV_SHOW("Shows","show"),
+    MUSIC("Music","music"),
+    BOOK("Books","book");
 
     private final String label;
+    private final String singular;
 
     public String getLabel() {
-        return label;
+        return this.label;
     }
 
-    SourceType(String label) {
+    public String getSingular() {
+        return this.singular;
+    }
+
+    SourceType(String label, String singular) {
         this.label = label;
+        this.singular = singular;
     }
 }
