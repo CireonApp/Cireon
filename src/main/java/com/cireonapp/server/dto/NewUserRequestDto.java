f04c1844aa11a33d76f6108844386b466f63a669
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-06-10 17:58:06 +0300

    Add content filtering for user creation.

--- src/main/java/com/cireonapp/server/dto/NewUserRequestDto.java
+++ src/main/java/com/cireonapp/server/dto/NewUserRequestDto.java
@@ -13,23 +13,27 @@ import jakarta.validation.constraints.Size;
 )
 public class NewUserRequestDto {
 
-    @NotNull(message="Username is required")
-    @NotBlank(message="Username is required")
+    @NotNull(message = "Username is required")
+    @NotBlank(message = "Username is required")
     @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Username can only contain letters, numbers, and underscores.")
-    @Size(min = 6,max = 18, message = "Username must be between 6 and 18 characters.")
-    @Schema(defaultValue = "john_doe",description = "Username of the new user")
+    @Size(min = 6, max = 18, message = "Username must be between 6 and 18 characters.")
+    @Schema(defaultValue = "john_doe", description = "Username of the new user")
     public String username;
 
-    @NotNull(message="Password is required")
-    @NotBlank(message="Password is required")
+    @NotNull(message = "Password is required")
+    @NotBlank(message = "Password is required")
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Password can only contain letters and numbers.")
-    @Size(min = 8,max=24, message = "Password must be at least 8 characters long.")
+    @Size(min = 8, max = 24, message = "Password must be at least 8 characters long.")
     @Schema(description = "Password of the new user", defaultValue = "johndoetheking123")
     public String password;
 
-    @NotNull(message="Display name is required")
-    @NotBlank(message="Display name is required")
+    @NotNull(message = "Display name is required")
+    @NotBlank(message = "Display name is required")
     @Schema(defaultValue = "John Doe", description = "Display name of the new user")
-    @Size(min = 2, max= 20, message = "Display name must be between 2 and 20 characters.")
+    @Size(min = 2, max = 20, message = "Display name must be between 2 and 20 characters.")
     public String displayName;
+
+    @NotNull(message = "Adult content filtering is required")
+    @Schema(defaultValue = "true", description = "Adult content filtering for new users.")
+    public Boolean allowAdultContent;
 }
