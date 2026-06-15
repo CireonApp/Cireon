e02b51cf29abffec3aa20b2f4472fa4913bf873b
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-06-16 01:10:33 +0300

    Add support for universal media object for player to support multiple source types for easier handling with thymeleaf.

--- src/main/java/com/cireonapp/server/domain/media/common/UniversalMediaForPlayer.java
+++ src/main/java/com/cireonapp/server/domain/media/common/UniversalMediaForPlayer.java
@@ -4,7 +4,8 @@ import com.cireonapp.server.domain.media.source.SourceType;
 
 public class UniversalMediaForPlayer {
     public String title;
-    public String hash;
+    public String id;
+    public String defaultFile;
     public SourceType type;
     public Integer season;
     public Integer episode;
