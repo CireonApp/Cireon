fb354c70968bdbe729c7cb43edb964b828dc2220
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-06-16 01:26:47 +0300

    Make media type guesser use ID instead of hash.

--- src/main/java/com/cireonapp/server/util/GuessMediaType.java
+++ src/main/java/com/cireonapp/server/util/GuessMediaType.java
@@ -4,8 +4,8 @@ import com.cireonapp.server.domain.media.movie.MovieManager;
 import com.cireonapp.server.domain.media.source.SourceType;
 
 public class GuessMediaType {
-    public static SourceType guess(String hash) {
-        if (MovieManager.get(hash).isPresent())
+    public static SourceType guess(String id) {
+        if (MovieManager.get(id) != null)
             return SourceType.MOVIE;
         return null;
     }
