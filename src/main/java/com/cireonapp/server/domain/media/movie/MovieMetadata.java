2722bd90cee387ee087ad3cf9c4e7004eb67839a
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-06-16 01:17:01 +0300

    Remove unused constructor in MovieMetadata.java

--- src/main/java/com/cireonapp/server/domain/media/movie/MovieMetadata.java
+++ src/main/java/com/cireonapp/server/domain/media/movie/MovieMetadata.java
@@ -1,16 +1,11 @@
 package com.cireonapp.server.domain.media.movie;
 
-import com.cireonapp.server.domain.media.common.Artwork;
 import com.cireonapp.server.domain.media.common.CommonMetadata;
-import com.cireonapp.server.domain.media.common.Genres;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.dizitart.no2.index.IndexType;
 import org.dizitart.no2.repository.annotations.Entity;
 import org.dizitart.no2.repository.annotations.Index;
-
-import java.util.Set;
-
 
 @Entity(value = "movieMetadata", indices = {
         @Index(fields = "title", type = IndexType.FULL_TEXT),
@@ -44,11 +39,6 @@ public class MovieMetadata extends CommonMetadata {
 
     public MovieMetadata() {
         super();
-    }
-
-    public MovieMetadata(int id, String title, String originalTitle, String description, String releaseDate, Artwork artworks, int runtime, Set<Genres> genres, Set<String> alternativeTitles, boolean adult, String tagline, long lastUpdated) {
-        super(id, title, originalTitle, description, artworks, runtime, genres, alternativeTitles, adult, tagline, lastUpdated);
-        this.releaseDate = releaseDate;
     }
 
     public String getReleaseDate() {
