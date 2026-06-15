55244256cffa0d6dbb08ea2c00d7f1875cdbf58c
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-06-16 01:20:35 +0300

    Add media info class for generated info by FFmpeg.

---
+++ src/main/java/com/cireonapp/server/domain/media/common/MediaInfoByFFmpeg.java
@@ -0,0 +1,77 @@
+package com.cireonapp.server.domain.media.common;
+
+public class MediaInfoByFFmpeg {
+    private Double duration;
+    private Double fps;
+    private String audioCodec;
+    private String videoCodec;
+    private Integer height;
+    private Integer width;
+
+    public MediaInfoByFFmpeg(Double duration, Double fps, String audioCodec, String videoCodec, Integer height, Integer width) {
+        this.duration = duration;
+        this.fps = fps;
+        this.audioCodec = audioCodec;
+        this.videoCodec = videoCodec;
+        this.height = height;
+        this.width = width;
+    }
+
+    public MediaInfoByFFmpeg() {
+    }
+
+    public Double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Double duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "MediaInfoByFFmpeg{" +
+                "duration=" + duration +
+                '}';
+    }
+
+    public Double getFps() {
+        return fps;
+    }
+
+    public void setFps(Double fps) {
+        this.fps = fps;
+    }
+
+    public String getAudioCodec() {
+        return audioCodec;
+    }
+
+    public void setAudioCodec(String audioCodec) {
+        this.audioCodec = audioCodec;
+    }
+
+    public String getVideoCodec() {
+        return videoCodec;
+    }
+
+    public void setVideoCodec(String videoCodec) {
+        this.videoCodec = videoCodec;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+}
