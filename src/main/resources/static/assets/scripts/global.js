b2f5be33121bc976b60b1c877ef8e6f45f67c965
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-06-16 01:25:58 +0300

    Add support for horizontal carousel dragging with mouse.

--- src/main/resources/static/assets/scripts/global.js
+++ src/main/resources/static/assets/scripts/global.js
@@ -2,3 +2,42 @@ document.addEventListener('contextmenu', (event) => {
     if (event.target.tagName === "INPUT") return;
     event.preventDefault();
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    const sliders = document.querySelectorAll('.horizontal-drag-container');
+
+// 2. Loop through each individual container
+    sliders.forEach((slider) => {
+        // Each slider gets its own unique tracking variables
+        let isDown = false;
+        let startX;
+        let scrollLeft;
+
+        slider.addEventListener('mousedown', (e) => {
+            isDown = true;
+            startX = e.pageX - slider.offsetLeft;
+            scrollLeft = slider.scrollLeft;
+            document.body.style.userSelect = 'none';
+        });
+
+        slider.addEventListener('mouseleave', () => {
+            isDown = false;
+            document.body.style.userSelect = 'auto';
+        });
+
+        slider.addEventListener('mouseup', () => {
+            isDown = false;
+            document.body.style.userSelect = 'auto';
+        });
+
+        slider.addEventListener('mousemove', (e) => {
+            if (!isDown) return;
+            e.preventDefault();
+
+            const x = e.pageX - slider.offsetLeft;
+            const walk = (x - startX) ;
+            slider.scrollLeft = scrollLeft - walk;
+        });
+    });
+})
