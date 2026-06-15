7cbcafef2f6a8fd5393098c9414119e572132a19
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2026-06-16 01:12:43 +0300

    Update source manager to support the new getter method rework for database repos.

--- src/main/java/com/cireonapp/server/domain/media/source/SourceManager.java
+++ src/main/java/com/cireonapp/server/domain/media/source/SourceManager.java
@@ -1,5 +1,6 @@
 package com.cireonapp.server.domain.media.source;
 
+import com.cireonapp.server.domain.media.movie.MovieManager;
 import com.cireonapp.server.initializer.Databases;
 import com.cireonapp.server.initializer.FileWatcher;
 import org.dizitart.no2.common.WriteResult;
@@ -21,19 +22,24 @@ public class SourceManager {
         Filter watchForChangesFilter = where("watchForChanges").eq(onlyWatchForChanges);
 
         if (onlyEnabled && onlyWatchForChanges) {
-            return Databases.sourceRepository.find(Filter.and(enabledFilter, watchForChangesFilter));
+            return Databases.getSourceRepository().find(Filter.and(enabledFilter, watchForChangesFilter));
         } else if (onlyEnabled) {
-            return Databases.sourceRepository.find(enabledFilter);
+            return Databases.getSourceRepository().find(enabledFilter);
         } else if (onlyWatchForChanges) {
-            return Databases.sourceRepository.find(watchForChangesFilter);
+            return Databases.getSourceRepository().find(watchForChangesFilter);
         }
 
-        return Databases.sourceRepository.find(Filter.ALL);
+        return Databases.getSourceRepository().find(Filter.ALL);
     }
 
     public static boolean create(Source source) {
         source.setId(UUID.randomUUID().toString()); //Force a random ID
-        WriteResult result = Databases.sourceRepository.insert(source);
+        WriteResult result = Databases.getSourceRepository().insert(source);
+
+        if (source.isEnabled()) {
+            if (source.getType() == SourceType.MOVIE)
+                MovieManager.handleMovieSourceUpdate(source);
+        }
 
         if (source.isWatchForChanges()) {
             FileWatcher.registerSource(source);
@@ -43,7 +49,7 @@ public class SourceManager {
     }
 
     public static Optional<Source> get(String id) {
-        Source source = Databases.sourceRepository.getById(id);
+        Source source = Databases.getSourceRepository().getById(id);
         return Optional.ofNullable(source);
     }
 
@@ -54,7 +60,8 @@ public class SourceManager {
      * @return Returns true if the source was deleted successfully, false otherwise.
      */
     public static boolean delete(Source source) {
-        WriteResult result = Databases.sourceRepository.remove(source);
+        WriteResult result = Databases.getSourceRepository().remove(source);
+        FileWatcher.unregisterSource(source.getId());
         return result.getAffectedCount() > 0;
     }
 
